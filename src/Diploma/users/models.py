115e84c82c87cc178fd57b5d86bf4fad7128d38f
Kliment Fis <kirilltrun2015@gmail.com>
2024-02-12 23:30:36 +0300

    Fix Users Model + User List

--- src/Diploma/users/models.py
+++ src/Diploma/users/models.py
@@ -6,4 +6,4 @@ class MyUser(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True)
     patronymic = models.CharField(max_length=75, blank=True)
     use_english = models.BooleanField(default=False, blank=True)
-    data_joined = models.DateTimeField(null=True)
+    data_joined = models.DateTimeField(null=True, blank=True)
