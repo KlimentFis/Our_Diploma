19d0eb02b0c79c0863762f16b33e550998c52b50
bipchik <lera.pena2004@gmail.com>
2024-04-27 02:09:22 +0300

    Страница регистрации

--- Diplom/Diplom/obj/Debug/netstandard2.0/Diplom.AssemblyInfo.cs
+++ Diplom/Diplom/obj/Debug/netstandard2.0/Diplom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Diplom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da72a906512d3bf92c37536951127c645b65668")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2816ba73f62ff2c06224b38d37d51b42649422b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Diplom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Diplom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
