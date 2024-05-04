1b3a644c79f7b6f83a98895fa4450e4afb054daf
bipchik <lera.pena2004@gmail.com>
2024-05-05 01:08:07 +0300

    Дизайн страницы профиля

--- Мобильное_приложение_для_изучения_иностранных_языков/Diplom/Pages/ProfilePage.xaml.cs
+++ Мобильное_приложение_для_изучения_иностранных_языков/Diplom/Pages/ProfilePage.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Plugin.Media.Abstractions;
+using Plugin.Media;
+
 
 namespace Diplom.Pages
 {
@@ -15,6 +19,47 @@ namespace Diplom.Pages
         public ProfilePage()
         {
             InitializeComponent();
+        }
+
+        [Obsolete]
+        private async void OnFramePhoto(object sender, EventArgs e)
+        {
+            var photoStream = await PickPhotoAsync();
+            if (photoStream != null)
+            {
+
+                // Отобразите выбранное изображение в элементе Image
+                Photo.Source = ImageSource.FromStream(() => photoStream);
+            }
+        }
+
+        private async Task<Stream> PickPhotoAsync()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                _ = DisplayAlert("Ошибка", $"Выбор фотографий не поддерживается на устройстве", "OK");
+                return null;
+            }
+
+            var options = new PickMediaOptions
+            {
+                // Размер выбранного изображения
+                PhotoSize = PhotoSize.Custom,
+                CustomPhotoSize = 10, // Укажите желаемый размер в процентах от оригинального размера
+                RotateImage = false,
+                MaxWidthHeight = 2000, // Максимальные размеры изображения (не обязательно)
+                SaveMetaData = true, // Сохранение метаданных (не обязательно)
+                CompressionQuality = 92 // Качество сжатия изображения (не обязательно)
+            };
+
+            var file = await CrossMedia.Current.PickPhotoAsync(options);
+
+            if (file == null)
+                return null;
+
+            return file.GetStream();
         }
     }
 }
