76343426506b9a6315847a00bd559deba833571f
Kliment Fis <kirilltrun2015@gmail.com>
2024-05-06 23:03:20 +0300

    Вернул использование sqlite3 в проект

--- Сайт_для_изучения_иностранных_языков/api/views.py
+++ Сайт_для_изучения_иностранных_языков/api/views.py
@@ -1,11 +1,33 @@
+from django.middleware.csrf import get_token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from users.models import MyUser
 from words.models import Word, Suggestion
 from .serializers import MyUserSerializer, WordSerializer, SuggestionSerializer
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
 from rest_framework import status
+
+def get_csrf_token(request):
+    return get_token(request)
+
+def user_signup(request):
+    if request.method == 'POST':
+        csrf_token = get_csrf_token(request)  # Используем нашу функцию для получения CSRF-токена
+        serializer = MyUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            response_data = {
+                'access': str(refresh.access_token),
+                'refresh': str(refresh)
+            }
+            response = Response(response_data, status=status.HTTP_201_CREATED)
+            response['X-CSRFToken'] = csrf_token  # Добавляем CSRF-токен в заголовок ответа
+            return response
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
@@ -56,7 +78,8 @@ def delete_user(request):
         try:
             user = MyUser.objects.get(username=username, password=password)
             user.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            # return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response({'detail': 'Пользователь успешно удален!!!'})
         except MyUser.DoesNotExist:
             return Response({'error': 'Enter the correct user nickname and password'}, status=status.HTTP_404_NOT_FOUND)
     else:
