991169eec594aedf08644fd183ce42e5aaf942dd
bipchik <lera.pena2004@gmail.com>
2024-05-07 19:24:19 +0300

    Дизайн страниц всех страниц тестов

--- Мобильное_приложение_для_изучения_иностранных_языков/Diplom/Pages/TestsTranslation.xaml.cs
+++ Мобильное_приложение_для_изучения_иностранных_языков/Diplom/Pages/TestsTranslation.xaml.cs
@@ -9,12 +9,46 @@ using Xamarin.Forms.Xaml;
 
 namespace Diplom.Pages
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class TestsTranslation : ContentPage
-	{
-		public TestsTranslation ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestsTranslation : ContentPage
+    {
+        private RadioButton lastCheckedRadioButton; // Хранит ссылку на последнюю выбранную радиокнопку
+
+        public TestsTranslation()
+        {
+            InitializeComponent();
+
+            // При нажатии на фрейм срабатывает RadioButton
+            Frame1.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => OnFrameTapped(RadioBtn1))
+            });
+
+            Frame2.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => OnFrameTapped(RadioBtn2))
+            });
+
+            Frame3.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => OnFrameTapped(RadioBtn3))
+            });
+        }
+
+        private void OnFrameTapped(RadioButton radioButton)
+        {
+            if (radioButton == lastCheckedRadioButton) // Если выбрана уже выбранная радиокнопка
+            {
+                radioButton.IsChecked = false; // Снимаем отметку
+                lastCheckedRadioButton = null; // Сбрасываем ссылку на последнюю выбранную радиокнопку
+            }
+            else
+            {
+                if (lastCheckedRadioButton != null)
+                    lastCheckedRadioButton.IsChecked = false; // Снимаем отметку с последней выбранной радиокнопки
+                radioButton.IsChecked = true; // Устанавливаем отметку на выбранную радиокнопку
+                lastCheckedRadioButton = radioButton; // Обновляем ссылку на последнюю выбранную радиокнопку
+            }
+        }
+    }
 }
--- Мобильное_приложение_для_изучения_иностранных_языков/Diplom/Pages/TestsWords.xaml.cs
+++ Мобильное_приложение_для_изучения_иностранных_языков/Diplom/Pages/TestsWords.xaml.cs
@@ -9,12 +9,46 @@ using Xamarin.Forms.Xaml;
 
 namespace Diplom.Pages
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class TestsWords : ContentPage
-	{
-		public TestsWords ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestsWords : ContentPage
+    {
+        private RadioButton lastCheckedRadioButton; // Хранит ссылку на последнюю выбранную радиокнопку
+
+        public TestsWords()
+        {
+            InitializeComponent();
+
+            // При нажатии на фрейм срабатывает RadioButton
+            Frame1.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => OnFrameTapped(RadioBtn1))
+            });
+
+            Frame2.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => OnFrameTapped(RadioBtn2))
+            });
+
+            Frame3.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => OnFrameTapped(RadioBtn3))
+            });
+        }
+
+        private void OnFrameTapped(RadioButton radioButton)
+        {
+            if (radioButton == lastCheckedRadioButton) // Если выбрана уже выбранная радиокнопка
+            {
+                radioButton.IsChecked = false; // Снимаем отметку
+                lastCheckedRadioButton = null; // Сбрасываем ссылку на последнюю выбранную радиокнопку
+            }
+            else
+            {
+                if (lastCheckedRadioButton != null)
+                    lastCheckedRadioButton.IsChecked = false; // Снимаем отметку с последней выбранной радиокнопки
+                radioButton.IsChecked = true; // Устанавливаем отметку на выбранную радиокнопку
+                lastCheckedRadioButton = radioButton; // Обновляем ссылку на последнюю выбранную радиокнопку
+            }
+        }
+    }
 }
