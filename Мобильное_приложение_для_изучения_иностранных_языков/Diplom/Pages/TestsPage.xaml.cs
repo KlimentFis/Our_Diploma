a92b83d4f28bb9980b3105b3ee9feeb5f2e696b9
bipchik <lera.pena2004@gmail.com>
2024-05-08 03:35:22 +0300

    WIP тест проверка текста

--- Мобильное_приложение_для_изучения_иностранных_языков/Diplom/Pages/TestsPage.xaml.cs
+++ Мобильное_приложение_для_изучения_иностранных_языков/Diplom/Pages/TestsPage.xaml.cs
@@ -45,15 +45,9 @@ namespace Diplom.Pages
         private async void OnFrameText(object sender, EventArgs e)
         {
 
-            if (!IsUserLoggedIn())
-            {
-                await Navigation.PushAsync(new LoginPage());
-                return;
-            }
-            else
-            {
+            
                 await Navigation.PushAsync(new TestsText());
-            }
+
         }
 
         private bool IsUserLoggedIn()
--- Мобильное_приложение_для_изучения_иностранных_языков/Diplom/Pages/TestsText.xaml.cs
+++ Мобильное_приложение_для_изучения_иностранных_языков/Diplom/Pages/TestsText.xaml.cs
@@ -1,20 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json.Linq;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace Diplom.Pages
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class TestsText : ContentPage
-	{
-		public TestsText ()
-		{
-			InitializeComponent ();
-		}
-	}
+    public partial class TestsText : ContentPage
+    {
+        public TestsText()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSendButtonClicked(object sender, EventArgs e)
+        {
+            string textToCheck = EditorTxt.Text;
+            List<string> errors = await CheckGrammarAsync(textToCheck);
+            if (errors.Count > 0)
+            {
+                // Вывод ошибок
+                _=DisplayAlert("Грамматические ошибки", string.Join("\n", errors), "OK");
+            }
+            else
+            {
+                // В случае отсутствия ошибок
+                await DisplayAlert("Проверка завершена", "Грамматических ошибок не обнаружено", "OK");
+            }
+        }
+
+        private async Task<List<string>> CheckGrammarAsync(string text)
+        {
+            List<string> errors = new List<string>();
+
+            // API URL для проверки грамматики
+            string apiUrl = "https://languagetool.org/api/v2/check";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Параметры запроса
+                    var parameters = new Dictionary<string, string>
+                    {
+                        { "language", "en-US" }, // Язык текста
+                        { "text", text } // Текст для проверки
+                    };
+
+                    // Отправка POST-запроса к API LanguageTool
+                    var response = await client.PostAsync(apiUrl, new FormUrlEncodedContent(parameters));
+
+                    // Обработка ответа
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Преобразование ответа в JSON
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        JObject jsonObject = JObject.Parse(jsonResponse);
+
+                        // Извлечение списка ошибок из JSON
+                        var matches = jsonObject["matches"];
+                        foreach (var match in matches)
+                        {
+                            errors.Add(match["message"].ToString());
+                        }
+                    }
+                    else
+                    {
+                        errors.Add("Ошибка при запросе к API LanguageTool");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errors.Add("Ошибка: " + ex.Message);
+            }
+
+            return errors;
+        }
+    }
 }
