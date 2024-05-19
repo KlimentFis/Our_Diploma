aa475e4be3b97a0b2cbd202028c38dae55c8b0ed
bipchik <lera.pena2004@gmail.com>
2024-05-19 19:08:17 +0300

    Авторизация проверка на несуществующего пользователя

--- Мобильное_приложение_для_изучения_иностранных_языков/Diplom/Pages/LoginPage.xaml.cs
+++ Мобильное_приложение_для_изучения_иностранных_языков/Diplom/Pages/LoginPage.xaml.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using static Diplom.config;
-
 
 namespace Diplom.Pages
 {
@@ -22,47 +20,60 @@ namespace Diplom.Pages
         {
             try
             {
-                string url = "http://test.bipchik.keenetic.pro/api/token/";
-                //string url = config.Our_addres;
+                string usersUrl = "http://test.bipchik.keenetic.pro/api/all_users/";
                 string username = LoginEntry.Text;
                 string password = PasswordEntry.Text;
-                string jsonData = $"{{\"username\":\"{username}\",\"password\":\"{password}\"}}";
 
                 using (HttpClient client = new HttpClient())
                 {
-                    HttpContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-                    HttpResponseMessage response = await client.PostAsync(url, content);
-
-                    if (response.IsSuccessStatusCode)
+                    // Fetch the list of all users
+                    HttpResponseMessage usersResponse = await client.GetAsync(usersUrl);
+                    if (usersResponse.IsSuccessStatusCode)
                     {
-                        string responseContent = await response.Content.ReadAsStringAsync();
+                        string usersContent = await usersResponse.Content.ReadAsStringAsync();
+                        var usersList = JsonConvert.DeserializeObject<string[]>(usersContent);
 
-                        // Распарсить responseContent в объект
-                        var responseObject = JsonConvert.DeserializeObject<dynamic>(responseContent);
+                        // Check if the entered username exists in the list
+                        if (Array.Exists(usersList, user => user == username))
+                        {
+                            string authUrl = "http://test.bipchik.keenetic.pro/api/token/";
+                            string jsonData = $"{{\"username\":\"{username}\",\"password\":\"{password}\"}}";
+                            HttpContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                            HttpResponseMessage authResponse = await client.PostAsync(authUrl, content);
 
-                        // Получить значения токенов из объекта
-                        string refreshToken = responseObject.refresh;
-                        string accessToken = responseObject.access;
+                            if (authResponse.IsSuccessStatusCode)
+                            {
+                                string responseContent = await authResponse.Content.ReadAsStringAsync();
+                                var responseObject = JsonConvert.DeserializeObject<dynamic>(responseContent);
 
-                        // Сохранить токены в хранилище приложения
-                        Application.Current.Properties["RefreshToken"] = refreshToken;
-                        Application.Current.Properties["AccessToken"] = accessToken;
-                        await Application.Current.SavePropertiesAsync();
+                                string refreshToken = responseObject.refresh;
+                                string accessToken = responseObject.access;
 
-                        // Перейти на другую страницу после успешной аутентификации
-                        await DisplayAlert("Успех", "Вы успешно вошли", "OK");
-//                      await Navigation.PushAsync(new UsersPage());
+                                Application.Current.Properties["RefreshToken"] = refreshToken;
+                                Application.Current.Properties["AccessToken"] = accessToken;
+                                await Application.Current.SavePropertiesAsync();
+
+                                await DisplayAlert("Успех", "Вы успешно вошли", "OK");
+                                // await Navigation.PushAsync(new UsersPage());
+                            }
+                            else
+                            {
+                                await DisplayAlert("Ошибка", $"Ошибка при выполнении запроса: {authResponse.StatusCode}", "OK");
+                            }
+                        }
+                        else
+                        {
+                            await DisplayAlert("Ошибка", "Пользователь с таким логином не существует", "OK");
+                        }
                     }
                     else
                     {
-                        // Вывод сообщения об ошибке в случае неудачного запроса
-                        await DisplayAlert("Ошибка", $"Ошибка при выполнении запроса: {response.StatusCode}", "OK");
+                        await DisplayAlert("Ошибка", "Ошибка при получении списка пользователей", "OK");
                     }
                 }
             }
             catch (Exception ex)
             {
-                // Вывод сообщения об ошибке в случае исключения
                 await DisplayAlert("Ошибка", $"Ошибка: {ex.Message}", "OK");
             }
         }
