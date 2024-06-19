8cfde9c49faefb9680016b48e3e0073bca39378c
Kliment Fis <kirilltrun2015@gmail.com>
2024-06-20 01:43:51 +0300

    Добавлен костыль для динамического поиска

--- Сайт_для_изучения_иностранных_языков/tests/urls.py
+++ Сайт_для_изучения_иностранных_языков/tests/urls.py
@@ -13,24 +13,46 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from tests.views import letter_verification, check_word, check_suggestion, tests, translateWord, about_us, text_to_audio
-from words.views import all_words
+from .views import letter_verification, check_word, check_suggestion, tests, translateWord, about_us, text_to_audio
+from words.views import all_words, search_view
 
 urlpatterns = [
     path('', tests, name='tests'),
     path('letter_verification/', letter_verification, name='letter_verification'),
     path('check_word/', check_word, name='check_word'),
     path('check_suggestion/', check_suggestion, name='check_suggestion'),
-    path('translate_word', translateWord, name='translateWord'),
-    path('all_words/', all_words, name="all_words"),
-    # path('text_to_audio/', text_to_audio, name="text_to_audio")
+    path('translate_word/', translateWord, name='translateWord'),
+    path('all_words/', all_words, name='all_words'),
+    path('search/', search_view, name='search'),
     path('text_to_audio/', text_to_audio, name='text_to_audio'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns = [
+#     path('', tests, name='tests'),
+#     path('letter_verification/', letter_verification, name='letter_verification'),
+#     path('check_word/', check_word, name='check_word'),
+#     path('check_suggestion/', check_suggestion, name='check_suggestion'),
+#     path('translate_word', translateWord, name='translateWord'),
+#     # path('all_words/', all_words, name="all_words"),
+#
+#
+#     # path('all_words/', all_words, name='all_words'),
+#     # path('search/', search_view, name='search'),
+#
+#     path('all_words/', all_words, name='all_words'),
+#     path('search/', search_view, name='search'),  # Проверьте, что этот маршрут существует
+#
+#     # path('text_to_audio/', text_to_audio, name="text_to_audio")
+#     path('text_to_audio/', text_to_audio, name='text_to_audio'),
+# ]
+#
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Сайт_для_изучения_иностранных_языков/words/views.py
+++ Сайт_для_изучения_иностранных_языков/words/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render
 from words.models import Word
 # Create your views here.
@@ -15,3 +16,13 @@ def all_words(request):
         "data": Word.objects.all()
     }
     return render(request, 'tests/all_words.html', context)
+
+def search_view(request):
+    q = request.GET.get('q', '')
+    if q:
+        results = Word.objects.filter(name__istartswith=q) | Word.objects.filter(translate__istartswith=q)
+    else:
+        results = Word.objects.filter(name__istartswith='a')
+
+    data = [{'id': item.id, 'name': item.name, 'translate': item.translate} for item in results]
+    return JsonResponse(data, safe=False)
